dd844534a731e067572468342e4a218471ca05f3
Francisco Sarabia <siglasconexion@hotmail.com>
2023-12-12 21:58:47 -0500

    completando cambios en control stock_detail y resumiendo funciones 12-12-23

--- controladores/control_stock_detail.js
+++ controladores/control_stock_detail.js
@@ -50,65 +50,100 @@ export const getStock_detail = async (req, res) => {
   res.json(resultGetOne);
 };
 
+/*
+const create_stock_detail = async (req) => {
+  try {
+    await Stock_detail.create({
+      // const resultNew =
+      id_stock_stock_detail: req.body.idstockstockdetail,
+      id_item_stock_detailsdasdfasdfasd: req.body.iditemstockdetail,
+      id_place_stock_detail: req.body.idplacestockdetail,
+      id_container_stock_detail: req.body.idcontainerstockdetail,
+      qty_container_stock_detail: req.body.qtycontainerstockdetail,
+      units_stock_detail: req.body.unitsstockdetail,
+      total_stock_detail: req.body.totalstockdetail,
+    });
+  } catch (error) {
+    // definir objeto de retorno 
+  }
+};
 export const createStock_detail = async (req, res) => {
   //console.log("req.body", req.body);
-  const resultNew = await Stock_detail.create({
-    id_stock_stock_detail: req.body.idstockstockdetail,
-    id_item_stock_detail: req.body.iditemstockdetail,
-    id_place_stock_detail: req.body.idplacestockdetail,
-    id_container_stock_detail: req.body.idcontainerstockdetail,
-    qty_container_stock_detail: req.body.qtycontainerstockdetail,
-    units_stock_detail: req.body.unitsstockdetail,
-    total_stock_detail: req.body.totalstockdetail,
-  });
-  console.log("cheqar el bdy", req.body.iditemstockdetail);
-  const resultNew2 = await Current_inventory.findOne({
-    where: {
-      id_item_current_inventory: req.body.iditemstockdetail,
-    },
-  });
-  return res.status(200).json(resultNew2);
+  try {
+   let resultadocreate = await create_stock_detail(req);
 
-  const resultNew3 = 0;
-  if (_.isEmpty(resultNew2)) {
-    resultNew3 = await Current_inventory.create({
+ */
+
+export const createStock_detail = async (req, res) => {
+  //console.log("req.body", req.body);
+  try {
+    await Stock_detail.create({
+      // const resultNew =
+      id_stock_stock_detail: req.body.idstockstockdetail,
+      id_item_stock_detailsdasdfasdfasd: req.body.iditemstockdetail,
+      id_place_stock_detail: req.body.idplacestockdetail,
+      id_container_stock_detail: req.body.idcontainerstockdetail,
+      qty_container_stock_detail: req.body.qtycontainerstockdetail,
+      units_stock_detail: req.body.unitsstockdetail,
+      total_stock_detail: req.body.totalstockdetail,
+    });
+
+    //console.log("cheqar el bdy", req.body.iditemstockdetail);
+    const resultNew2 = await Current_inventory.findOne({
+      where: {
+        id_item_current_inventory: req.body.iditemstockdetail,
+      },
+    });
+    let convertResultNew2 = resultNew2?.toJSON(); // let variable = .toJSON
+    let previousTotal = convertResultNew2.total_current_inventory;
+    //return res.status(200).json(resultNew2);
+    let resultNew3 = 0; // estaba declarada con const y no se podia reasignar abajo  cheaear porque la defini
+    if (_.isEmpty(convertResultNew2)) {
+      // resultNew2
+      resultNew3 = await Current_inventory.create({
+        id_stock_current_inventory: req.body.idstockstockdetail,
+        id_item_current_inventory: req.body.iditemstockdetail,
+        total_current_inventory: req.body.totalstockdetail,
+      });
+
+      Object.entries(resultNew3).length === 0
+        ? res.json({ message: "Register is not created" })
+        : res.json({ message: resultNew3 });
+      return; // aqui retornamos la respuesta al from del create
+    }
+    let totalNew = parseInt(req.body.totalstockdetail) + previousTotal;
+    /* console.log(totalNew);
+    console.log(previousTotal);
+    console.log(totalNew, previousTotal); */
+
+    let obj = {
       id_stock_current_inventory: req.body.idstockstockdetail,
       id_item_current_inventory: req.body.iditemstockdetail,
-      total_current_inventory: req.body.totalstockdetail,
+      total_current_inventory: totalNew,
+    };
+    const resultUpdate = await Current_inventory.update(obj, {
+      where: {
+        id_item_current_inventory: req.body.iditemstockdetail,
+      },
     });
-
-    Object.entries(resultNew3).length === 0
-      ? res.json({ message: "Register is not created" })
-      : res.json({ message: resultNew3 });
-    //return // aqui retornamos la respuesta al from del create
+    if (resultUpdate[0] === 1) {
+      res.status(200).json({
+        message: "Status Update successfully",
+        resultUpdate: resultUpdate,
+      });
+    } else {
+      res.status(400).json({
+        error: "valor demasiado grande",
+        message: "Status not successfully",
+        resultUpdate: resultUpdate,
+      });
+    }
+  } catch (error) {
+    console.log("aquir muestra la descripcion de error message", error.message);
+    console.log("aquir el error stack", error.stack);
+    console.log("aca el error erros", error.errors);
+    console.log("aqui va el error de la funcion Create_stock_detail", error);
   }
-  //console.log("variable prueba ", prueba);
-  //console.log("total Viejo", totalViejo);
-  let totalFinal = req.body.totalstockdetail + totalViejo;
-  let obj = {
-    id_stock_current_inventory: req.body.idstockstockdetail,
-    id_item_current_inventory: req.body.iditemstockdetail,
-    total_current_inventory: totalFinal,
-  };
-  const resultUpdate = await Current_inventory.update(obj, {
-    where: {
-      id_item_current_inventory: req.body.iditemstockdetail,
-    },
-  });
-  if (resultUpdate[0] === 1) {
-    res.status(200).json({
-      message: "Status Update successfully",
-      resultUpdate: resultUpdate,
-    });
-  } else {
-    //throw { status: res.status, statusText: res.statusText };
-    res.status(400).json({
-      error: "valor demasiado grande",
-      message: "Status not successfully",
-      resultUpdate: resultUpdate,
-    });
-  }
-
   // actualizo la tabla
   // respondo al from
 };
