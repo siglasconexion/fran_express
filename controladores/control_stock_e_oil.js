bfc19dafd8f73ad6f267a9d8015582d51fc7bf7f
Francisco Sarabia <siglasconexion@hotmail.com>
2024-03-12 00:00:33 -0400

    terminando el cierre de inventario essential oils

--- controladores/control_stock_e_oil.js
+++ controladores/control_stock_e_oil.js
@@ -2,6 +2,7 @@ const { Stock_e_oil } = require("../db/models/stock_e_oil.js");
 const { Stock_detail_e_oil } = require("../db/models/stock_detail_e_oil.js");
 const { Oil_input } = require("../db/models/oil_input.js");
 const { Essential_oil } = require("../db/models/essential_oil.js");
+const { QueryTypes } = require("sequelize");
 const db = require("../db/conn.js");
 
 const getStocks_e_oil = async (req, res) => {
@@ -107,7 +108,78 @@ const deleteStock_e_oil = async (req, res) => {
     console.log(err.stack);
   }
 };
-const getStock_e_oil_closed = async (req, res) => {};
+const getStock_e_oil_closed = async (req, res) => {
+  // rutas - routes
+  let variablefinal = req.params.variable;
+  console.log("HEY CA LA VARIABLE", variablefinal);
+  console.log("variable sola del objeto params", variablefinal);
+  const data = await db.sequelize.query(
+    `SELECT  * from stock_detail_e_oil where id_stock_stock_detail_e_oil = ${variablefinal} ORDER BY id_e_oil_stock_detail_e_oil`,
+    { type: QueryTypes.SELECT }
+  ); //
+  //  let convertResultNew2 = data?.toJSON();
+  //console.log("primera consulta", data);
+  const register = [];
+  for (const el of data) {
+    let rest = 0;
+    let id = 0;
+    let iditem = el.id_e_oil_stock_detail_e_oil;
+    let spent = parseFloat(el.spent_stock_detail_e_oil);
+    let spent0 = parseFloat(el.spent_stock_detail_e_oil);
+    let resultNew2 = await db.sequelize.query(
+      ` UPDATE essential_oil SET otro_stock = otro_stock - ${spent} WHERE essential_oil.id_essential_oil = ${iditem}`,
+      { type: QueryTypes.UPDATE }
+    );
+    let resultNew3 = await db.sequelize.query(
+      ` SELECT * FROM oil_input WHERE id_essential_oil_oil_input = ${iditem} ORDER BY date_received_oil_input`,
+      { type: QueryTypes.SELECT }
+    );
+    //console.log("resultNew3", resultNew3);
+    for (const el2 of resultNew3) {
+      id = el2.id_oil_input;
+      console.log("id_container_oil_input", el2.id_container_oil_input);
+      console.log("id_user_oil_input", el2.id_user_oil_input);
+      for (let i = 0; spent > 0; i++) {
+        id = el2.id_oil_input;
+        if (spent > parseFloat(el2.stock_oil_input)) {
+          rest = 0;
+          spent = spent - parseFloat(el2.stock_oil_input);
+          register.push({
+            stock_oil_input: rest,
+            in_use_oil_input: 0,
+            finish_oil_input: 1,
+            date_end_oil_input: null,
+            where: id,
+            spent0: spent0,
+            spent: spent,
+            iditem: iditem,
+            rest: rest,
+            stock: el2.stock_oil_input,
+          });
+          break;
+        } else {
+          rest = parseFloat(el2.stock_oil_input) - spent;
+          spent = 0;
+          register.push({
+            stock_oil_input: rest,
+            in_use_oil_input: 1,
+            finish_oil_input: 0,
+            date_end_oil_input: null,
+            comentario: "aca lo hizo al primero",
+            where: id,
+            spent0: spent0,
+            spent: spent,
+            iditem: iditem,
+            rest: rest,
+            stock: el2.stock_oil_input,
+          });
+        }
+      }
+    }
+    //console.log("error para que se pare", errorparaquesepare);
+  }
+  console.log("register", register);
+};
 
 module.exports = {
   getStocks_e_oil,
